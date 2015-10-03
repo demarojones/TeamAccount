a8bfdeecab19b862f483c933e773c549780c3b90
Demaro Jones <demaro4@gmail.com>
2015-10-03 03:54:36 -0400

    Updated entities and created datacontext

--- TeamAccount.DTO/User.cs
+++ TeamAccount.DTO/User.cs
@@ -10,13 +10,9 @@ namespace TeamAccount.DTO
     {
         public int Id { get; set; }
         public string UserName { get; set; }
-        public string Password { get; set; }
         public string LastName { get; set; }
         public string FirstName { get; set; }
         public string TeamId { get; set; }
         public string imageName { get; set; }
-
-        public virtual Team Team { get; set; }
-
     }
 }
---
+++ TeamAccount.Repository/Repositories/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamAccount.DAL.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        IQueryable<T> GetAll();
+        T GetById(int id);
+        void Add(T entry);
+        void update(T entry);
+        void delete(T entry);
+        void deleteById(int id);
+        void Detach(T entity);
+    }
+}
